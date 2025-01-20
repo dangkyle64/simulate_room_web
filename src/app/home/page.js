7f03f7f5c31b6e0852a300d998ac49c337bc8cb6
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-01-20 13:28:00 -0800

    update back to async for testing
    author: dangkyle64
    date: 1/20/2025

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,23 +1,12 @@
 import { fetchRestData } from "../lib/restapi";
 
-export async function getServerSideProps() {
-    
-    const data = await fetchRestData();
-  
-    return {
-      props: { data }, 
-    };
-};
+export default async function Home() {
+  const data = await fetchRestData(); 
 
-export default function Home() {
-
-    console.log('The data:', data);
-
-    return (
-        <div>
-            <h1>Data from Rest Backend</h1>
-            {/* Render the message */}
-            <p>{data.message}</p>
-        </div>
-    );
-};
+  return (
+    <div>
+      <h1>Data from Rest Backend</h1>
+      <p>{data.message}</p>
+    </div>
+  );
+}
