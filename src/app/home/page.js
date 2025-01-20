bb4190582a300a81cc67d5b5beb47bf59f48c869
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-01-20 13:11:31 -0800

    Update page.js
    author: dangkyle64
    date: 1/20/2025

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,9 +1,19 @@
 import { fetchRestData } from "../lib/restapi";
 
+export async function getServerSideProps() {
+    
+    const data = await fetchRestData();
+  
+    return {
+      props: { data }, 
+    };
+};
+
 export default async function Home() {
 
-    const data = await fetchRestData();
+    const data = await fetchRestData({ data });
     console.log('The data:', data);
+    
     return (
         <div>
             <h1>Data from Rest Backend</h1>
